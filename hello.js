12cd4362e09250258567ce12bc33e9954615226d
user <user@domain>
2014-03-12 03:20:38 +0000

    foo

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World from Node.js\n');
+http.createServer(function (req, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('Hello World from Node.js\n');
     console.log("Hello from node!");
 }).listen(port);
